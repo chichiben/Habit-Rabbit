b0e497228738ae74a75c50703b15a91c6a71bce8
johntrinhcode@gmail.com <johntrinhcode@gmail.com>
2020-04-18 23:41:25 -0400

    removed activity bar

--- app/src/main/java/com/example/habitrabbit/MainActivity.kt
+++ app/src/main/java/com/example/habitrabbit/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.habitrabbit
 
 import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
 import android.widget.RelativeLayout
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
@@ -10,9 +12,11 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_main)
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
 
@@ -24,7 +28,6 @@ class MainActivity : AppCompatActivity() {
                 R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
             )
         )
-        setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
 
         val habitButton = findViewById<RelativeLayout>(R.id.habit1)
